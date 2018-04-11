8395f768273b8a70ae35ffdc75f5d29b4c06dfb8
HugoRocha <hrocha97@gmail.com>
2018-04-11 15:18:14 +0100

    Get dependencies specifications(version,licenseUrl...)

--- MSBuildPlugin/DependencyAnalyzer.cs
+++ MSBuildPlugin/DependencyAnalyzer.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.Build.Evaluation;
 using Microsoft.Build.Framework;
 using Microsoft.Build.Utilities;
+using System.IO;
+using NuGet.Packaging;
 
 namespace MSBuildPlugin
 {
@@ -13,13 +15,25 @@ namespace MSBuildPlugin
         {
             BuildEngine.LogMessageEvent(new BuildMessageEventArgs("Plugin is running... ", "", _id, MessageImportance.High));
             BuildEngine.LogMessageEvent(new BuildMessageEventArgs("Project File: " + ProjectFilePath, "", _id, MessageImportance.High));
-            Project project = new Project(ProjectFilePath);
+            return ValidateProjectDependencies(new Project(ProjectFilePath));
+        }
+
+        private bool ValidateProjectDependencies(Project project)
+        {
             foreach (ProjectItem item in project.AllEvaluatedItems)
             {
                 if (item.HasMetadata("HintPath"))
                 {
-                    string value = item.GetMetadataValue("HintPath");
-                    BuildEngine.LogMessageEvent(new BuildMessageEventArgs("Package found: " + value, "", _id, MessageImportance.High));
+                    string hintPath = item.GetMetadataValue("HintPath");
+                    string packageDir = Path.GetFullPath(Path.Combine(hintPath, @"..\..\..\"));
+                    string packageFile = new DirectoryInfo(packageDir).Name + ".nupkg";
+                    string packageFilePath = Path.Combine(packageDir, packageFile);
+                    BuildEngine.LogMessageEvent(new BuildMessageEventArgs("Package found: " + packageFilePath, "", _id, MessageImportance.High));
+                    NuspecReader packageReader = new PackageArchiveReader(packageFilePath).NuspecReader;
+                    string licenseUrl = packageReader.GetLicenseUrl();
+                    string version = packageReader.GetVersion().OriginalVersion;
+                    string id = packageReader.GetId();
+                    BuildEngine.LogMessageEvent(new BuildMessageEventArgs(string.Format("Version: {0}; Id: {1}; License: {2}", version, id, licenseUrl), "", _id, MessageImportance.High));
                 }
             }
             return true;
